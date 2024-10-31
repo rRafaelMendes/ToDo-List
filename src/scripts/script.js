ff9311d248e64bdd3a57c1bd5854f3e348a090f4
Renato Mendes <165093605+rRafaelMendes@users.noreply.github.com>
2024-10-31 16:51:02 -0300

    Finalizando as funções de armazenamento das variáveis da lista

---
+++ src/scripts/script.js
@@ -0,0 +1,35 @@
+const taskList = document.getElementById("taskList")
+const taskInput = document.getElementById("taskInput")
+
+
+function addTask() {
+    const taskText = taskInput.value.trim();
+    if (taskText !== "") {
+
+        const maxText = taskText.substring(0, 50)     
+
+        const li = document.createElement("li");
+        li.innerHTML = `
+
+        <span>${maxText}</span>
+        <button class="editButton" onclick="editTask(this)" > Editar </button>
+        <button class="deleteButton" onclick="deleteTask" > Remover </button>
+        `;
+        taskList.appendChild(li)
+        taskInput.value = ""
+    }
+
+}
+function editTask(button) {
+    const li = button.parentElement;
+    const span = li.querySelector("span")
+    const newText = prompt("Editar meta: ", span.textContent)
+    if(newText !== null && newText.trim() !== "") {
+        span.textContent = newText.trim()
+    }
+}
+
+function deleteTask(button) {
+    const li = button.parentElement
+    taskList.removeChild(li)
+} 
